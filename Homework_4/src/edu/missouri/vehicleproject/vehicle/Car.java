e79ef69031308ed51a3ba267752f44c505e0574e
jljcb <jljcb@DellXPS13>
2024-02-24 17:28:51 -0600

    implemented method for calculateFuelEfficiency and public void startEngine

--- Homework_4/src/edu/missouri/vehicleproject/vehicle/Car.java
+++ Homework_4/src/edu/missouri/vehicleproject/vehicle/Car.java
@@ -15,22 +15,22 @@ public class Car extends Vehicle
 	@Override
 	public double calculateMaintenaceCost(double distance)
 	{
-		// TODO 
-		
-		return 0;
+		long currentYear = java.time.Year.now().getValue();	
+		return distance * this.mass * (currentYear - this.modelYear) * this.cylinders * 0.0005;
 	}
 
 	@Override
 	public double calculateFuelEfficiency(double distance, double fuelPrice)
 	{
-		// TODO 
-		
-		return 0.0;
+		if (distance == 0) {
+			return 0;		
+		}
+		return (this.cylinders * this.gasTankCapacity * this.fuelPrice) / (distance * 0.003);
 	}
 
 	@Override
 	public void startEngine()
 	{
-		// TODO
+		System.out.printIn("Engine starts with PUSHSTART.")
 	}
 }
